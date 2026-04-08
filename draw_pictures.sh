ae8f282793cc7c701f21ab474351ca6efd7be5be
afra818 <60651664+afra818@users.noreply.github.com>
2026-04-08 14:51:17 +0100

    f

--- draw_pictures.sh
+++ draw_pictures.sh
@@ -8,15 +8,15 @@
 
 
 # robust
-srun python analyze_calibration_plots.py \
-  --baseline_npz /scratch/hpc/07/zhang303/O-TPT-main/analysis_npz/DTD_baseline_vitb32_tecoa_eps_1_pgd_eps0.00392156862745.npz \
-  --otpt_npz /scratch/hpc/07/zhang303/O-TPT-main/analysis_npz/DTD_tpt_otpt_vitb32_tecoa_eps_1_pgd_eps0.00392156862745.npz \
-  --output_dir /scratch/hpc/07/zhang303/O-TPT-main/analysis_plots/DTD_tecoa \
-  --mode robust
-
-# clean
-# srun  python analyze_calibration_plots.py \
+# srun python analyze_calibration_plots.py \
 #   --baseline_npz /scratch/hpc/07/zhang303/O-TPT-main/analysis_npz/DTD_baseline_vitb32_tecoa_eps_1_pgd_eps0.00392156862745.npz \
 #   --otpt_npz /scratch/hpc/07/zhang303/O-TPT-main/analysis_npz/DTD_tpt_otpt_vitb32_tecoa_eps_1_pgd_eps0.00392156862745.npz \
 #   --output_dir /scratch/hpc/07/zhang303/O-TPT-main/analysis_plots/DTD_tecoa \
-#   --mode clean
+#   --mode robust
+
+# clean
+srun  python analyze_calibration_plots.py \
+  --baseline_npz /scratch/hpc/07/zhang303/O-TPT-main/analysis_npz/DTD_baseline_vitb32_tecoa_eps_1_pgd_eps0.00392156862745.npz \
+  --otpt_npz /scratch/hpc/07/zhang303/O-TPT-main/analysis_npz/DTD_tpt_otpt_vitb32_tecoa_eps_1_pgd_eps0.00392156862745.npz \
+  --output_dir /scratch/hpc/07/zhang303/O-TPT-main/analysis_plots/DTD_tecoa \
+  --mode clean
