cf9158f61f208adbac04ab2f5f76803a6dd61724
afra818 <60651664+afra818@users.noreply.github.com>
2026-04-27 16:42:23 +0100

    f

--- scripts/ucat_run.sh
+++ scripts/ucat_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=t1d1
+#SBATCH --job-name=t1d0.5
 #SBATCH -p gpu-medium
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -45,7 +45,7 @@ workers=8
 # ===== New method hyperparameters =====
 lambda_tpt=1
 # 原始 TPT 的 marginal entropy loss 权重
-lambda_dir=1
+lambda_dir=0.5
 # Dirichlet consistency loss 的权重。
 dir_temp=1.0
 # 这是把 logits 映射成 Dirichlet 参数时用的温度。
